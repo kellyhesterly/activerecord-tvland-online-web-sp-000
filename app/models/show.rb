8de5d6e97e02374090f43aeb1d2e2990b5cead56
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-09 23:07:33 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
-    "#{self.actors.first.first_name} #{self.actors.first.last_name}"
+  
     binding.pry
   end
 end
